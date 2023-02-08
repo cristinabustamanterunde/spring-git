fa69821729b5987457c4e89289fab9ac92098afd
“cristinabustamanterunde” <cristina.bustamanterunde@gmail.com>
2023-02-08 09:41:24 +0100

    Entidad Usuario

---
+++ src/main/java/com/example/springgit/User.java
@@ -0,0 +1,13 @@
+package com.example.springgit;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+	
+	public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+}
